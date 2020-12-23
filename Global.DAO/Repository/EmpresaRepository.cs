ee95c6ae05c147047cc00ebeb37754b25baae465
Junior possari <junior.possari@pro4tech.com.br>
2020-12-23 10:26:40 -0300

    repositórios e serviços

---
+++ Global.DAO/Repository/EmpresaRepository.cs
@@ -0,0 +1,17 @@
+﻿using Global.DAO.Context;
+using Global.DAO.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Global.DAO.Repository
+{
+    public class EmpresaRepository : GenericRepository<Empresa, GlobalContext>
+    {
+
+        public EmpresaRepository() :base(){ }
+
+        public EmpresaRepository(GlobalContext context) : base(context) { }
+    }
+}
---
+++ Global.DAO/Service/AreaInteresseService.cs
@@ -0,0 +1,72 @@
+﻿using Global.DAO.Context;
+using Global.DAO.Model;
+using Global.DAO.Procedure.Models;
+using Global.DAO.Repository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Global.DAO.Service
+{
+    public class AreaInteresseService : IDisposable
+    {
+        private AreaInteresseRepository Repository { get; set; }
+
+        public AreaInteresseService()
+        {
+
+            Repository = new AreaInteresseRepository();
+
+        }
+
+        public AreaInteresseService(GlobalContext context)
+        {
+            Repository = new AreaInteresseRepository(context);
+        }
+
+
+        public AreaInteresse Buscar(int Id) 
+        {
+
+            return Repository.Get(x => x.Id == Id).FirstOrDefault();
+
+        }
+
+        public bool Salvar(AreaInteresse Dados)
+        {
+            
+            bool resultado = Repository.Insert(Dados);
+            return resultado;
+
+        }
+
+        public bool Editar(AreaInteresse Dados)
+        {
+
+            bool resultado = Repository.Update(Dados);
+
+            return resultado;
+
+        }
+
+        public bool Excluir(int Id)
+        {
+            var dados = Repository.GetByID(Id);
+            bool resultado = Repository.Delete(dados);
+
+            return resultado;
+        }
+
+        public GlobalContext GetContext()
+        {
+            return Repository.GetContext();
+        }
+
+        public void Dispose()
+        {
+            Repository.Dispose();
+        }
+    }
+}
