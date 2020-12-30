7803ae7843ac15063cb31b40c5bf3cb5594dc2e8
Pico Araújo <paulo.araujo@pro4tech.com.br>
2020-12-30 12:19:57 -0300

    entidades atualizadas

--- Global.API/ViewModel/Vaga.cs
+++ Global.API/ViewModel/Vaga.cs
@@ -12,11 +12,14 @@ namespace Global.API.ViewModel
         public string NomeCargo { get; set; }
         public string NomeEmpresa { get; set; }
         public string Salario { get; set; }
-        public string Modalidade  { get; set; }
+        public string Modalidade { get; set; }
         public string Cidade { get; set; }
-        public string Estado{ get; set; }
+        public string Estado { get; set; }
         public string Requisitos { get; set; }
         public string Beneficios { get; set; }
+        public string Favotitado { get; set; }
+
+
 
 
 
