08b2d26a4e40e95892ba5883cb25e0b24626c8c4
Pico Araújo <paulo.araujo@pro4tech.com.br>
2021-03-10 18:54:08 -0300

    Revert "add dev profile"
    This reverts commit 0c982f4142e446653bfa25fa09750fd16ee2c4cf.

--- Global.DAO/Context/GlobalContext.cs
+++ Global.DAO/Context/GlobalContext.cs
@@ -3,7 +3,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Global.DAO.Model;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Global.DAO.Procedure.Models;
 
 namespace Global.DAO.Context
 {
